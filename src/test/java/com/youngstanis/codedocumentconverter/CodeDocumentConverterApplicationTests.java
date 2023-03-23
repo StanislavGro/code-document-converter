daebe9d1a53da992624d72d64b9fd3607c44139c
Stanislav Grokhotov <grokhotov@isands.ru>
2023-03-23 16:15:04 +0300

    initial commit

---
+++ src/test/java/com/youngstanis/codedocumentconverter/CodeDocumentConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngstanis.codedocumentconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeDocumentConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
